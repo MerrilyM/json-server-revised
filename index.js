ae2ef7e1a37abe773e531a23e2514ab5d5161eb7
MerrilyM <150508076+MerrilyM@users.noreply.github.com>
2024-05-21 10:34:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server"); //impo
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+ 
